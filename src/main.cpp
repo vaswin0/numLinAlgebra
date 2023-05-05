403e0e123fd6b888ec4da75c739950b52ca7b5aa
vaswin0 <vaswin0137@gmail.com>
2023-05-05 15:21:29 +0530

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,32 @@
 #include "Matrix.h"
 #include <iostream>
+#include <tuple>
 
 
 int main(){
 
-Matrix p(4,4);
+Matrix m(3,3);
 //p.shape();
 
-p.initMatrix();
+m.initMatrix();
 
 //p.shape();
 //std::cout << p.transpose() << std::endl;
 //std::cout << p << std::endl;
-std::cout << p << std::endl;
+std::cout << m << std::endl;
 //std::cout << gaussEli(p) << std::endl;
-std::cout << lu(p) << std::endl;
+Matrix l, u, p, a;
+auto result =  lu(m);
+
+l = std::get<0>(result);
+u = std::get<1>(result) ;
+p = std::get<2>(result) ;
+a = std::get<3>(result) ;
+
+std::cout << l << std::endl;
+std::cout << u << std::endl;
+std::cout << p << std::endl;
+std::cout << a << std::endl;
 //std::cout <<  (3*p*p).swapRows(0,2).scaleRow(3,2) << std::endl;
 
 
